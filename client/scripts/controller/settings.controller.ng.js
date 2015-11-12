65c4d824ed4d10376b96d7edbf811122776c4297
Sasa314 <zhangyusi34@gmail.com>
2015-11-12 00:22:16 -0500

    Create settings.controller.ng.js

---
+++ client/scripts/controller/settings.controller.ng.js
@@ -0,0 +1,15 @@
+ angular
+  .module('COMP231-Project')
+  .controller('SettingsCtrl', SettingsCtrl);
+ 
+function SettingsCtrl($scope, $meteor, $state) {
+  $scope.logout = logout;
+ 
+  ////////////
+ 
+  function logout() {
+    $meteor.logout().then(function () {
+      $state.go('login');
+    });
+  }
+}
